908408213c05ba9bf81565213bcead5fe28b09ac
ZamDimon <zamdmytro@gmail.com>
2023-12-11 00:12:11 +0200

    :checkered_flag: finish task for homeworks 3-4

--- src/main/java/org/turnBasedFighting/game/Army.java
+++ src/main/java/org/turnBasedFighting/game/Army.java
@@ -1,15 +1,22 @@
 package org.turnBasedFighting.game;
 
+import java.util.ArrayDeque;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Queue;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class Army implements Iterable<IUnit> {
+    private Queue<IUnit> troops = new ArrayDeque<>();
+
     public Army addUnits(Supplier<IUnit> unitFactory, int number) {
+        IntStream.rangeClosed(1, number).forEach(i -> troops.add(unitFactory.get()));
         return this;
     }
 
     public Army addUnits(UnitClass unitClass, int number) {
-        return this;
+        return this.addUnits(unitClass::make, number);
     }
 
     @Override
@@ -20,12 +27,16 @@ public class Army implements Iterable<IUnit> {
     private class FirstAliveIterator implements Iterator<IUnit> {
         @Override
         public boolean hasNext() {
-            return false;
+            while (!troops.isEmpty() && !troops.peek().isAlive()) {
+                troops.poll();
+            }
+            return !troops.isEmpty();
         }
 
         @Override
         public IUnit next() {
-            return null;
+            if (!hasNext()) throw new NoSuchElementException();
+            return troops.peek();
         }
     }
 }
